b5c84f9f480535dd96bb22220415bcf4c64e4e61
Jenil Gajjar <68738624+JenilGajjar20@users.noreply.github.com>
2023-08-18 16:16:11 +0530

    single-number

--- Cpp/single-number/solution.cpp
+++ Cpp/single-number/solution.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+    int result = 0;
+    for (int num : nums)
+        result ^= num; // XOR
+
+    return result;
+}
 
 int main()
 {
-    cout << "In Progress";
+    vector<int> nums = {4, 2, 1, 2, 1};
+
+    int num = singleNumber(nums);
+    cout << "Number appeared once: " << num << endl;
+
     return 0;
 }
