1107f6617354c1230a24ef16038e3cddd747672f
Jenil Gajjar <jenil20gajjar@gmail.com>
2023-08-24 13:37:46 +0530

    WIP

--- Cpp/migratory-birds/solution.cpp
+++ Cpp/migratory-birds/solution.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
-int migratoryBirds(int arr[], int n) {}
+int migratoryBirds(vector<int> &arr)
+{
+    // map<int, int> birdCount;
+
+    // for (int bird : arr)
+    //     birdCount[bird]++;
+
+    // int maxCount = 0;
+    // int mostFrequentBirds = 1;
+
+    // for (const auto &entry : birdCount)
+    // {
+    //     cout << "Entry First: " << entry.first << endl;
+    //     cout << "Entry second: " << entry.second << endl;
+    //     if (entry.second > maxCount)
+    //     {
+    //         maxCount = entry.second;
+    //         mostFrequentBirds = entry.first;
+    //     }
+    // }
+
+    // return mostFrequentBirds;
+}
 
 int main()
 {
-    int size;
-    cout << "Size of an array: ";
-    cin >> size;
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
 
-    vector<int> arr[size];
-    for (int i = 0; i < size; i++)
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
     {
         cout << "Enter elements: ";
         cin >> arr[i];
     }
 
-    migratoryBirds(arr, size);
+    int result = migratoryBirds(arr);
+    cout << "Result: " << result << endl;
     return 0;
 }
