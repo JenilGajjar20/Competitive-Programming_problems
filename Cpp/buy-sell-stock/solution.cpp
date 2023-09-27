8df120b23601d633a16d235cffda63577c0d9966
arghya1912 <84338935+arghya1912@users.noreply.github.com>
2023-09-27 00:22:39 -0700

    Update solution.cpp
    I have updated the code from:
    T.C = O(n^2) to T.C = O(n)

--- Cpp/buy-sell-stock/solution.cpp
+++ Cpp/buy-sell-stock/solution.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
+#include<vector>
+#include<limits.h>
 using namespace std;
 
-int maxProfit(int price[], int start, int end)
-{
-
-    if (end <= start)
-    {
-        return 0;
+int maxProfit(vector<int>& prices) {
+    int mini = INT_MAX, ans = 0;
+    
+    for(auto &x : prices) {
+        mini = min(mini, x);
+        ans = max(ans, x - mini);
     }
-    int profit = 0;
-    int i, j;
-    for (i = start; i < end; i++)
-    {
-        for (j = i + 1; j <= end; j++)
-        {
-            if (price[j] > price[i])
-            {
-                int current_profit = price[j] - price[i] + maxProfit(price, start, i - 1) + maxProfit(price, j + 1, end);
-
-                profit = max(profit, current_profit);
-            }
-        }
-    }
-    return profit;
+    return ans;
 }
 
 int main()
 {
-    int price[] = {110, 130, 220, 500, 140, 35, 65};
+    vector<int> price = {110, 130, 220, 500, 140, 35, 65};
     int size = sizeof(price) / sizeof(price[0]);
-    cout << "Profit: " << maxProfit(price, 0, size - 1);
+    cout << "Profit: " << maxProfit(price);
     return 0;
 }
