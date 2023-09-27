3c2dee281042b6113716a239bca45c3e14ff540e
abhimaurya-dev <abhishekmaurya.official@gmail.com>
2023-09-27 14:57:05 +0530

    Updated-solution: Changed time complexity from O(nlongn) to O(n)

--- Cpp/min-max-sum/solution.cpp
+++ Cpp/min-max-sum/solution.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
+
 using namespace std;
 
 int minMaxSum(int arr[], int n)
 {
-    // Sorting the array integers
-    sort(arr, arr + n);
+    // Variable declarations
+    long max_element = LONG_MIN;
+    long min_element = LONG_MAX;
+    long long min_sum = 0;
+    long long max_sum = 0;
+    long long arr_sum = 0;
 
-    long max = 0;
-    long min = 0;
-    int i;
-    for (i = 0; i < n - 1; i++)
-        min += arr[i];
+    // loop to find sum of array and to find minimum and maximum element in the array
+    for (int i = 0; i < n; i++)
+    {
+        arr_sum += arr[i];
+        if (arr[i] > max_element)
+        {
+            max_element = arr[i];
+        }
 
-    for (i = 1; i < n; i++)
-        max += arr[i];
+        if (arr[i] < min_element)
+        {
+            min_element = arr[i];
+        }
+    }
 
-    cout << min << " " << max << endl;
+    // subtrating maximum element in array from array sum to find the min sum that can be found with n-1 element of array
+    min_sum = arr_sum - max_element;
+    // subtrating minimum element in array from array sum to find the max sum that can be found with n-1 element of array
+    max_sum = arr_sum - min_element;
+
+    cout << min_sum << " " << max_sum << endl;
 }
 
 int main()
