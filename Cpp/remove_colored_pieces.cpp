7223c27485800048e594be2188c254d48486bfee
Niraj Kumar <98650481+Niraj-1729@users.noreply.github.com>
2023-10-17 01:16:05 +0530

    Create remove_colored_pieces.cpp

---
+++ Cpp/remove_colored_pieces.cpp
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/description/
+
+class Solution {
+public:
+    bool winnerOfGame(string colors) {
+        int countAAA=0;
+        int countBBB=0;
+
+        for(int i=1; i+1<colors.length(); ++i){
+            if(colors[i-1]==colors[i] && colors[i]==colors[i+1]){
+                if(colors[i]=='A'){
+                    ++countAAA;
+                }
+                else 
+                   ++countBBB;
+            }
+        }
+        return countAAA> countBBB;
+    }
+};
