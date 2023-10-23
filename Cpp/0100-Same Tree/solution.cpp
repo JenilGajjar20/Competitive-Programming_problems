e371ffeed16871aa643b7595349dcd50e219c275
Adithya <adithyranand@gmail.com>
2023-10-23 16:58:44 +0530

    Added 0100-Same tree

---
+++ Cpp/0100-Same Tree/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // If both nodes are NULL, they are identical
+        if (p == NULL && q == NULL) {
+            return true;
+        }
+        // If only one of the nodes is NULL, they are not identical
+        if (p == NULL || q == NULL) {
+            return false;
+        }
+        // Check if values are equal and recursively check left and right subtrees
+        if (p->val == q->val) {
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+        }
+        // Values are not equal, they are not identical
+        return false;
+    }
+};
