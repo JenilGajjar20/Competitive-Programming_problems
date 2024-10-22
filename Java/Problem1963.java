90925c75468262f3244ef6c3bc8948453e84450b
Devesh A <143060274+Th3C0d3Mast3r@users.noreply.github.com>
2024-10-22 19:24:46 +0530

    Created Problem1963.java

---
+++ Java/Problem1963.java
@@ -0,0 +1,37 @@
+// the URL of the Problem:- https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced/
+
+import java.util.*;
+public class Problem1963 {
+    static Scanner obj = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        run();
+    }
+
+    private static void run() {
+        String str = obj.nextLine();  // takes the input String consisting mainly of [ and ]
+        int ans = minSwaps(str);
+        System.out.println(ans);
+    }
+
+    private static int minSwaps(String s) {
+        Stack<Character> stack = new Stack<>();
+        int answer = 0;
+        int imbalance = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '[') {
+                stack.push(s.charAt(i));
+            } else {
+                if (!stack.isEmpty() && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    imbalance++;
+                }
+            }
+        }
+
+        answer = (imbalance + 1) / 2;
+        return answer;
+    }
+}
